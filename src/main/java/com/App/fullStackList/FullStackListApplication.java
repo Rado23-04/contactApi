66dfff0cce3e8c0979fe079f3263e4ff8adfba13
Rado23-04 <hei.rado.2@gmail.com>
2025-03-06 10:54:41 +0300

    first commit

---
+++ src/main/java/com/App/fullStackList/FullStackListApplication.java
@@ -0,0 +1,13 @@
+package com.App.fullStackList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/App/fullStackList/FullStackListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.fullStackList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
